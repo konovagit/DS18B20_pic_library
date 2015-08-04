0f748f8db4ba2b6585bfea00aba970763c04a763
konovagit <corentincorde@orange.fr>
2015-08-04 09:50:51 +0100

    Librairie terminée. Test et lecture température OK

--- DS18B20.h
+++ DS18B20.h
@@ -11,11 +11,34 @@ extern "C" {
 #define Write_Data LATC0
 #define Read_Data RC0
 #define Direction_Data TRISC0
+#define ReadROM 0x33
+#define MatchROM 0x55
+#define SkipROM 0xCC
+#define SearchROM 0xF0
+#define AlarmSearch 0xEC
+#define ConvertT 0x44
+#define ReadScratchpad 0xBE
+#define WriteScratchpad 0x4E
+#define CopyScratchpad 0x48
+#define RecallEE 0xB8
+#define ReadPowerSupply 0xB4
+#define DevicesNumber 2
 
+extern unsigned char DeviceAddress[DevicesNumber][8];
     
     
  /*Prototypes des fonctions*/
 unsigned char init_sensor(void);
+void write_Bit(unsigned char WriteArg);
+void write_Byte(unsigned char data);
+unsigned char read_Bit(void);
+unsigned char read_Byte(void);
+unsigned char address_Device(unsigned char Adress[]);
+unsigned char search_Devices(void);
+float read_temp(unsigned char DeviceAdress);
+void delay100ms(void);
+void delay50ms(void);
+void delay750ms(void);
 
 
 
